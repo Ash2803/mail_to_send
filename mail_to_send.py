27a29da3b578c0d3447703a06d793e22ab9f59fb
Ashot Abazyan <ashot.93@mail.ru>
2022-06-07 20:30:12 +0300

    Added comments above the code

--- mail_to_send.py
+++ mail_to_send.py
@@ -2,6 +2,7 @@ import os
 import smtplib
 from dotenv import load_dotenv
 
+# Сохраняем все изменяемые данные в переменные
 web_adrress = "https://dvmn.org/referrals/a0CrgaQsT8dZkyBadQ1xRjQu8NBgs4CEpCtysnMT/"
 friend_name = "Саша"
 my_name = "Ашот"
@@ -9,7 +10,6 @@ mail_from = "ashotabazyan@yandex.ru"
 mail_to = "a.abazyan@bk.ru"
 letter_subject = "Приглашение!"
 content_type = '''text/plain; charset="UTF-8"'''
-
 letter = f"""\
 From: {mail_from}
 To: {mail_to}
@@ -33,12 +33,15 @@ Content-Type: {content_type}
 Регистрируйся → {web_adrress}  
 На модули, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
 
+# Устанавливаем кодировку письма для корректного отображения
 letter = letter.encode("UTF-8")
 
+# Подгружаем переменные окруженя
 load_dotenv()
 my_secret_login = os.environ['YANDEX_LOGIN']
 my_secret_password = os.environ['YANDEX_PASSWORD']
 
+# Отправляем письмо и закрываем сессию
 server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
 server.login(my_secret_login, my_secret_password)
 server.sendmail(mail_from, mail_to, letter)
